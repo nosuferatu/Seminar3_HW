77a04ab5c46d4f471e5bfe2a41cb07922ed7bd95
nosuferatu <kuznetsovpf@gmail.com>
2023-04-19 23:57:26 +0300

    add task3 solution

--- Program.cs
+++ Program.cs
@@ -57,3 +57,28 @@ int ReadInt(string message)
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
 }
+
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.Clear();
+
+int numberN = ReadInt("Введите число N: ");
+
+for (int i = 1; i <= numberN; i++)
+{ 
+    Console.Write($"{i*i*i} ");
+}
+
+
+
+// Функция ввода сообщения
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
